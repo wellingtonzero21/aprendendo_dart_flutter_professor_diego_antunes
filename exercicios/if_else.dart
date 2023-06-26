6653d8fab29cc54a3bfcb70d3fec93e7fb888d69
Wellington Andrade <wellington021.andrade@gmail.com>
2023-06-26 19:01:46 -0300

    exercicios

---
+++ exercicios/if_else.dart
@@ -0,0 +1,12 @@
+main() {
+  int numero = 8;
+
+  if(numero > 0) {
+    print("O numero é positivo");
+  } else if (numero < 0) {
+    print("i numero é negativo");
+  } else {
+  print('o valor é 0');  
+  }
+  
+}
