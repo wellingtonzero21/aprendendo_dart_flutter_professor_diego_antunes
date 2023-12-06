09fa6fe360ecb85de31adb3b7af3a75bca6d04ea
Wellington Andrade <wellington021.andrade@gmail.com>
2023-12-06 15:32:51 -0300

    add hive

--- flutter_aula1/flutter_professor_diego/lib/controllers/theme_controller.dart
+++ flutter_aula1/flutter_professor_diego/lib/controllers/theme_controller.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:hive/hive.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends GetxController {
   var isDark = false.obs;
@@ -8,13 +12,16 @@ class ThemeController extends GetxController {
     'light' : ThemeMode.light,
     'dark' : ThemeMode.dark,
   };
-  SharedPreferences? prefs;
+  //SharedPreferences? prefs;
 
   static ThemeController get to => Get.find();
 
   loadThemeMode() async {
-    prefs = await SharedPreferences.getInstance();
-    String themeText = prefs?.getString('theme') ?? 'light';
+    //prefs = await SharedPreferences.getInstance();
+    //String themeText = prefs?.getString('theme') ?? 'light';
+    Directory dir = await getApplicationDocumentsDirectory();
+    var box = await Hive.openBox('preferencias', path: dir.path);
+    String themeText = box.get('theme') ?? 'light';
     isDark.value = themeText == "dark" ? true : false;
     setMode(themeText);
   }
@@ -22,8 +29,10 @@ class ThemeController extends GetxController {
   Future setMode(String themeText) async {
     ThemeMode? themeMode = themeModes[themeText];
     Get.changeThemeMode(themeMode!);
-    prefs = await SharedPreferences.getInstance();
-    await prefs?.setString('theme', 'themeText');
+    //prefs = await SharedPreferences.getInstance();
+    //await prefs?.setString('theme', 'themeText');
+    var box = await Hive.openBox('preferencias');
+    await box.put('theme', themeText);
   }
 
   changeTheme() {
